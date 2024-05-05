5ca65fe6789704617733652e2301c94a67bd75d6
Gyanendra <gyanen56@gmail.com>
2024-05-05 13:21:04 +1000

    able to delete item an edit mode

--- script.js
+++ script.js
@@ -3,55 +3,69 @@ const input = document.querySelector('.input')
 const container = document.querySelector('.listcontainer')
 const clearBtn = document.querySelector('.clear-btn')
 const subBtn = document.querySelector('.submit-btn')
+
 let editMode =false;
 let editId;
+let groceries =[]
 form.addEventListener("submit", (e) => {
-  e.preventDefault()
-  let id = new Date().getTime()
-  if (!input.value) {
-    alert('Please enter value', 'red')
-    return
-  }
-  if(input.value && editMode){
-    const lists = document.querySelectorAll('.edit-btn')
-    let itemTOEdit;
-    lists.forEach((li)=>{
-        if(li.dataset.id===editId){
-            itemTOEdit=li
-        }
-    })
-    // Editing the list 
-    itemTOEdit.parentNode.children[0].textContent=input.value
-    alert('Item Edited', 'green')
-    setDefault()
-    editMode=false
-    return
-  }
-  
-  const element = document.createElement('div')
-  element.classList.add('list')
-  element.innerHTML = ` <p>${input.value}</p> 
-                 <button class="delete-btn" data-id="${id}"><i class="fa-solid fa-trash"></i></button>
-                  <button class="edit-btn" data-id="${id}"><i class="fa-solid fa-pen-to-square"></i></button>
-             `
-
-//    Delete event start
-  const deleteItem = element.querySelector('.delete-btn')
-  deleteItem.addEventListener('click', deletingItem)
-//    Delete event end
-//    Edit event start
-  const editItem = element.querySelector('.edit-btn')
-  editItem.addEventListener('click',editingItem)
-//    Edit event end
-  container.appendChild(element)
-  alert('Item added on list', 'green')
-  setDefault()
+  e.preventDefault();
+   addItem();
 })
 
 
 
+// addItem 
+function addItem(){
+   let id = new Date().getTime()
+   if (!input.value) {
+     alert('Please enter value', 'red')
+     return
+   }
+   if (input.value && editMode) {
+     const lists = document.querySelectorAll('.edit-btn')
+     let itemToEdit
+     lists.forEach((li) => {
+       if (li.dataset.id === editId) {
+         itemToEdit = li
+       }
+     })
+     if (!itemToEdit) {
+       return
+     }
+     // Editing the list
+     itemToEdit.parentNode.children[0].textContent = input.value
+     alert('Item Edited', 'green')
+     setDefault()
+     editMode = false
+     return
+   }
 
+   const element = document.createElement('div')
+   element.classList.add('list')
+   element.innerHTML = ` <p>${input.value}</p> 
+                 <button class="delete-btn" data-id="${id}"><i class="fa-solid fa-trash"></i></button>
+                  <button class="edit-btn" data-id="${id}"><i class="fa-solid fa-pen-to-square"></i></button>
+             `
 
+   //    Delete event start
+   const deleteItem = element.querySelector('.delete-btn')
+   deleteItem.addEventListener('click', deletingItem)
+   //    Delete event end
+
+   //    Edit event start
+   const editItem = element.querySelector('.edit-btn')
+   editItem.addEventListener('click', editingItem)
+   //    Edit event end
+
+   container.appendChild(element)
+   alert('Item added on list', 'green')
+
+   //   saving item to localStorage
+   groceries.push({ id: id, value: input.value })
+   addToLocalStorage(groceries)
+
+   setDefault()
+}
 
 // clearing all items
 clearBtn.addEventListener('click',()=>{
@@ -60,39 +74,46 @@ clearBtn.addEventListener('click',()=>{
     alert('Empty List', 'red')
 })
 
+
 // delete an Item
 function deletingItem(e){
-  let items = document.querySelectorAll('.delete-btn')
-  let item 
-   items.forEach((link)=>{
-       if(link.dataset.id ===e.currentTarget.dataset.id){
-        item=link
-       }
-   })
-  item.parentNode.remove();
-   items = document.querySelectorAll('.delete-btn')
-  if(items.length<1){
-    clearBtn.classList.remove('show')
-     alert('Empty List', 'red')
-  }else{
-        alert('Item Deleted', 'red')
-    
-  }
- 
+        let items = document.querySelectorAll('.delete-btn')
+        let item 
+        items.forEach((link)=>{
+            if(link.dataset.id ===e.currentTarget.dataset.id){
+                item=link
+            }
+        })
+        item.parentNode.remove();
+        items = document.querySelectorAll('.delete-btn')
+        if(items.length<1){
+            clearBtn.classList.remove('show')
+            alert('Empty List', 'red')
+            console.log('here')
+        }else{
+                alert('Item Deleted', 'red')
+            
+        }
+        if(editMode){
+         subBtn.textContent='Submit'
+         input.value = ''
+         editMode=false
+         addItem();
+        }
+      
+
 }
-// edit an Item
+
+    // edit an Item
 function editingItem(e){
-// e.currentTarget is edit btn, previous element sibling is delete btn
-// previous element sibling is list text that need to edit
-input.value =e.currentTarget.previousElementSibling.previousElementSibling.textContent
-subBtn.textContent='Edit'
+        // e.currentTarget is edit btn, previous element sibling is delete btn
+        // previous element sibling is list text that need to edit
+        input.value =e.currentTarget.previousElementSibling.previousElementSibling.textContent
+        subBtn.textContent='Edit'
 
-// setting adding mode true so when form submitted it will work as edit mode
-editMode=true;
-editId=e.currentTarget.dataset.id;
-
-
-
+        // setting adding mode true so when form submitted it will work as edit mode
+        editMode=true;
+        editId=e.currentTarget.dataset.id;
 }
 
 // setting to default form
@@ -116,3 +137,13 @@ function alert(message , color){
     },1000)
    
 }
+
+// localStorage
+    function addToLocalStorage(a){
+        localStorage.setItem('lists', JSON.stringify(a))
+        console.log(a)
+    }
+
+    function getFromLocalStorage(){
+    return JSON.parse(localStorage.getItem('lists'))
+    }
